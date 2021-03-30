c745fcd7eff3a61ab7f04b822a6aeae58f93999f
Will Spencer <willspencer16@gmail.com>
2021-03-30 16:06:52 +0100

    Added list space and sign in page and navigation buttons

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra/base'
 
 class Makersbnb < Sinatra::Base
+  enable :sessions
 
   get '/' do
-      "Testing Infrastructure"
+    erb(:home)
   end
 
   get '/sign_up' do
@@ -11,11 +12,33 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/sign_up' do
+    session[:username] = params[:username]
+    redirect '/welcome'
+  end
+
+  get '/sign_in' do
+    erb(:sign_in)
+  end
+
+  post '/sign_in' do
+    session[:username] = params[:username]
     redirect '/welcome'
   end
 
   get '/welcome' do
+    @username = session[:username]
     erb(:welcome)
   end
 
+  get '/listings' do
+    erb(:listings)
+  end
+
+  get '/listings/new' do
+    erb(:list_space)
+  end
+
+  post '/listings/new' do
+    redirect '/listings'
+  end
 end
