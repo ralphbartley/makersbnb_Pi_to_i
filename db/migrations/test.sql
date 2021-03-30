00f74b7cc698bd80c44d1339033dccb9c53280bc
David Greaves <dm_greaves@msn.com>
2021-03-30 11:30:16 +0100

    db migrations

---
+++ db/migrations/test.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE "makersbnb_test";
+
+\c makersbnb_test;
+
+CREATE TABLE users(id SERIAL PRIMARY KEY,
+username VARCHAR(60),
+password VARCHAR(240));
+
+CREATE TABLE spaces(id SERIAL PRIMARY KEY,
+name VARCHAR(60),
+description VARCHAR(240),
+daily_price int4,
+owner_id INTEGER REFERENCES users (id));
+
+CREATE TABLE bookings(id SERIAL PRIMARY KEY,
+space_id INTEGER REFERENCES
+spaces (id), date VARCHAR(60));
+
+CREATE TABLE dates_available(id SERIAL PRIMARY KEY,
+space_id INTEGER REFERENCES
+spaces(id), date VARCHAR(60));
