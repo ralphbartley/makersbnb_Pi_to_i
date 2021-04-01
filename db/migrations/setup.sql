6e6d13273041375bbd71a7a378c452a7d842f772
ralphbartley <ralphgbartley@gmail.com>
2021-04-01 15:05:31 +0100

    setup.sql changes for demo

--- db/migrations/setup.sql
+++ db/migrations/setup.sql
@@ -4,17 +4,32 @@ TRUNCATE users CASCADE;
 SELECT setval('users_id_seq', 1);
 SELECT setval('spaces_id_seq', 1);
 
-INSERT INTO users (username, password) VALUES('Test', 'password');
-INSERT INTO users (username, password) VALUES('Will', 'spencer');
-INSERT INTO users (username, password) VALUES('Ralph', 'bartley');
-INSERT INTO users (username, password) VALUES('Max', 'strivens');
-INSERT INTO users (username, password) VALUES('David', 'greaves');
+INSERT INTO users (username, password)
+  VALUES('Test', 'password');
+INSERT INTO users (username, password)
+  VALUES('Will', 'spencer');
+INSERT INTO users (username, password)
+  VALUES('Ralph', 'bartley');
+INSERT INTO users (username, password)
+  VALUES('Max', 'strivens');
+INSERT INTO users (username, password)
+  VALUES('David', 'greaves');
 
-INSERT INTO spaces (name, description, daily_price, owner_id) VALUES('House', '4 bedrooms with swimming pool', '100', '4');
-INSERT INTO spaces (name, description, daily_price, owner_id) VALUES('Cottage', '3 bedrooms with a thatched roof', '156', '3');
-INSERT INTO spaces (name, description, daily_price, owner_id) VALUES('Flat', '1 bedroom with balcony and sea view', '300', '4');
-INSERT INTO spaces (name, description, daily_price, owner_id) VALUES('Bungalow', '2 bedrooms with jacuzzi', '90', '5');
-INSERT INTO spaces (name, description, daily_price, owner_id) VALUES('Mansion', '10 rooms with butler', '999', '6');
+INSERT INTO spaces (name, description, daily_price, owner_id)
+  VALUES('House', '4 bedrooms with swimming pool', '100', '3');
+INSERT INTO spaces (name, description, daily_price, owner_id)
+  VALUES('Cottage', '3 bedrooms with a thatched roof', '156', '3');
+INSERT INTO spaces (name, description, daily_price, owner_id)
+  VALUES('Flat', '1 bedroom with balcony and sea view', '300', '4');
+INSERT INTO spaces (name, description, daily_price, owner_id)
+  VALUES('Bungalow', '2 bedrooms with jacuzzi', '90', '5');
+INSERT INTO spaces (name, description, daily_price, owner_id)
+  VALUES('Mansion', '10 rooms with butler', '999', '6');
+
+INSERT INTO dates_available (space_id, date) VALUES('3', '1');
+INSERT INTO dates_available (space_id, date) VALUES('3', '2');
+INSERT INTO dates_available (space_id, date) VALUES('3', '3');
+INSERT INTO dates_available (space_id, date) VALUES('3', '4');
 
 INSERT INTO dates_available (space_id, date) VALUES('2', '1');
 INSERT INTO dates_available (space_id, date) VALUES('2', '2');
